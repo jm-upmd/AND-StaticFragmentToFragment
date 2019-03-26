015790eb836547e05457bdc9dd3b3c8ca6817a9e
Jose Manuel <jmdominguez@aytoalcobendas.org>
2019-03-26 12:26:42 +0100

    Versión inicial

--- app/src/main/java/com/example/jose/fragmenttofragment/BlueFragment.java
+++ app/src/main/java/com/example/jose/fragmenttofragment/BlueFragment.java
@@ -2,6 +2,8 @@ package com.example.jose.fragmenttofragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -20,12 +22,17 @@ public class BlueFragment extends Fragment {
         // En este mentodo se hace el inflado del fragmento y se crean las referencias
         // a sus views componentes.
         View v = inflater.inflate(R.layout.fragment_blue, container, false);
-
-
-        // La view que contiene el fragmento
-        mTextView = v.findViewById(R.id.textview);
-
         return v;
+    }
+
+    // This event is triggered soon after onCreateView().
+    // onViewCreated() is only called if the view returned from onCreateView() is non-null.
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // La view que contiene el fragmento
+        mTextView = view.findViewById(R.id.textview);
     }
 
     // Este es un método público que la actividad puede usar para comunicarse directamente con
--- app/src/main/java/com/example/jose/fragmenttofragment/GreenFragment.java
+++ app/src/main/java/com/example/jose/fragmenttofragment/GreenFragment.java
@@ -2,6 +2,8 @@ package com.example.jose.fragmenttofragment;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,31 +13,12 @@ import android.widget.Button;
 public class GreenFragment extends Fragment {
     private OnGreenFragmentListener mCallback;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_green, container, false);
-
-        Button button = v.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String message = "¡Hola Azul! Yo soy Verde.";
-
-
-                // Lamada al metodo de la interface implementado en la activiadad
-                mCallback.mensajeDesdeFragmentoVerde(message);
-            }
-        });
-
-        return v;
-    }
 
     //Esta es la interface que la Activity implementará para que
     // este fragmento pueda comunicarse con ella
     public interface OnGreenFragmentListener {
         void mensajeDesdeFragmentoVerde(String text);
     }
-
 
     // Este método, que es el primero en ejecutarse en el ciclo de vida del fragment,
     // asugura que la Activity ha implementado nuestro listener y que no es null
@@ -51,13 +34,41 @@ public class GreenFragment extends Fragment {
             mCallback = (OnGreenFragmentListener) context;
         } else {
             throw new RuntimeException(context.toString()
-                    + " must implement OnGreenFragmentListener");
+                    + " la actividad debe implemantar la interface OnGreenFragmentListener");
         }
     }
 
+    // Hace el inflate del layout del fragment
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_green, container, false);
+        return v;
+    }
+
+
+    // De ejecuta justo después del onCreateView. Aquí se crean las referencias a los views y demás
+    // objetos del layout del fragment, se implementan sus listeners, ...
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = "¡Hola Azul! Yo soy Verde.";
+
+                // Lamada al metodo de la interface implementado en la activiadad
+                mCallback.mensajeDesdeFragmentoVerde(message);
+            }
+        });
+    }
+
+
+    // This method is called when the fragment is no longer connected to the Activity
+    // Any references saved in onAttach should be nulled out here to prevent memory leaks.
     @Override
     public void onDetach() {
         super.onDetach();
-        mCallback = null;
+        mCallback = null; // Para que limpie el objeto el recolector de basura y evitar memory lieak.
     }
 }
